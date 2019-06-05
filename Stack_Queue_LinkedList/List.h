67859d4e6f1a4f4653d2e3289b5141e28e7b2149
omercse26 <omercse26@gmail.com>
2019-06-04 21:03:41 -0500

    updated to C14

--- Stack_Queue_LinkedList/List.h
+++ Stack_Queue_LinkedList/List.h
@@ -8,75 +8,160 @@
 #ifndef LIST_H
 #define	LIST_H
 #include <iostream>
-#include "List_Node.h"
+#include <memory>
+
+//#include "List_Node.h"
 
 using namespace std;
+
+//template <typename T>
+//class Iterator;
+
 template <typename T>
-
-class Iterator;
-
-class List
+struct ListNode
 {
-protected:
-  typedef List_Node<T> Node;
-  Node *root;
-  
-public:  
-  List(): root (NULL) {};
-  ~List(){root->deleteAllNodes();}
-  
-  void insert(T);
-  Node* deleteNode(T);
-  void print();
-  Node* first() { return root; } 
-  Iterator begin() { return new Iterator(this); }
+	T data;
+	std::unique_ptr<ListNode> next;
+	ListNode(T a) : data(a), next(nullptr) {}
+
+	~ListNode()
+	{
+		cout << "Destructing node " << data << endl;
+	}
 };
 
 template <typename T>
-void List<T>::insert(T data)
+class List
 {
-   if (root == NULL)   
-      root = new Node(data);
-   else
-   {
-     Node* temp = new Node(data,root);
-	 root = temp;
-   }
+	using Node = ListNode<T>;
+	using NodePtr = std::unique_ptr<Node>;
+
+	NodePtr root;
+public:
+	List() : root(nullptr) {}
+
+	void insertFront(T data)
+	{
+		NodePtr node = std::make_unique<Node>(data);
+		node->next = std::move(root);
+		root = std::move(node);
+	}
+
+	NodePtr remove(T data)
+	{
+		Node* prev = nullptr;
+		Node* curr = root.get();
+
+		while (curr != nullptr)
+		{
+			if (curr->data != data)
+			{
+				prev = curr;
+				curr = curr->next.get();
+				continue;
+			}
+
+			if (curr != root.get())
+			{
+				auto temp = std::move(prev->next);
+				prev->next = std::move(temp->next);
+				return temp;
+			}
+			else
+			{
+				auto temp = std::move(root);
+				root = std::move(temp->next);
+				return temp;
+			}
+		}
+
+		return nullptr;
+	}
+
+	void print()
+	{
+		for (Node* node = root.get(); node != nullptr; node = node->next.get())
+		{
+			cout << node->data << " ";
+		}
+
+		cout << endl;
+	}
+};
+
+void testdriver() 
+{	
+	List<int> list;
+	list.insertFront(1);
+	list.insertFront(2);
+	list.insertFront(3);
+
+	list.remove(1);
+
+	list.print();
 }
 
-template <typename T>
-List_Node<T>* List<T>::deleteNode(T data)
-{
-   if (root == NULL)   
-      return NULL;
-   
-   Node *temp = root;
-   Node *prev = NULL;
-   while (temp)
-   {
-      if (temp->data == data)
-	  {
-		 if (temp == root)
-			root = temp->next;
-		 else
-			prev->next = temp->next;
-	         delete temp;
-		 return prev;
-	  }	  
-	  prev = temp;
-	  temp = temp->next;	     
-   }
-   return NULL;
-}
-
-template <typename T>
-void List<T>::print()
-{
-
-  for (Node *temp = root; temp != NULL; temp = temp->next)
-     cout << temp->data << " ";
-  cout << endl;
-}
+//class List
+//{
+//protected:
+//  typedef List_Node<T> Node;
+//  Node *root;
+//  
+//public:  
+//  List(): root (NULL) {};
+//  ~List(){root->deleteAllNodes();}
+//  
+//  void insert(T);
+//  Node* deleteNode(T);
+//  void print();
+//  Node* first() { return root; } 
+//  Iterator begin() { return new Iterator(this); }
+//};
+//
+//template <typename T>
+//void List<T>::insert(T data)
+//{
+//   if (root == NULL)   
+//      root = new Node(data);
+//   else
+//   {
+//     root = new Node(data,root);
+//   }
+//}
+//
+//template <typename T>
+//List_Node<T>* List<T>::deleteNode(T data)
+//{
+//   if (root == NULL)   
+//      return NULL;
+//   
+//   Node *temp = root;
+//   Node *prev = NULL;
+//   while (temp)
+//   {
+//      if (temp->data == data)
+//	  {
+//		 if (temp == root)
+//			root = temp->next;
+//		 else
+//			prev->next = temp->next;
+//	         delete temp;
+//		 return prev;
+//	  }	  
+//	  prev = temp;
+//	  temp = temp->next;	     
+//   }
+//   return NULL;
+//}
+//
+//template <typename T>
+//void List<T>::print()
+//{
+//
+//  for (Node *temp = root; temp != NULL; temp = temp->next)
+//     cout << temp->data << " ";
+//  cout << endl;
+//}
 
 #endif	/* LIST_H */
 
